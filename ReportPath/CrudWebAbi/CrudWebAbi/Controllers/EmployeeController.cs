2b27fb3991dc600313fb8cb3f9ea09bcf5809de7
Barathraj <barathbeiem@gmail.com>
2023-06-06 11:46:41 +0530

    add crud

---
+++ ReportPath/CrudWebAbi/CrudWebAbi/Controllers/EmployeeController.cs
@@ -0,0 +1,79 @@
+﻿using CrudWebAbi.Database;
+using CrudWebAbi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudWebAbi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeDbContext EmployeeDbContext;
+        public EmployeeController(EmployeeDbContext employeeDbContext)
+        {
+            this.EmployeeDbContext = employeeDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmployee() 
+        {
+            var Employees = await EmployeeDbContext.Employees.ToListAsync();
+            return Ok(Employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee([FromBody] Employee model)
+        {
+            model.Id = new Guid();
+
+            await EmployeeDbContext.Employees.AddAsync(model);
+            await EmployeeDbContext.SaveChangesAsync();
+            return Ok(model);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id , [FromBody] Employee model)
+        {
+             var employee = await EmployeeDbContext.Employees.FirstOrDefaultAsync(a => a.Id == id);
+            if (employee != null) 
+            {
+                employee.Name = model.Name;
+                employee.MobileNo = model.MobileNo;
+                employee.EmailId = model.EmailId;
+                await EmployeeDbContext.SaveChangesAsync();
+
+                return Ok(employee);
+            }
+
+            else
+            {
+                return NotFound("Employee not found");
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id)
+        {
+            var employee = await EmployeeDbContext.Employees.FirstOrDefaultAsync(a => a.Id == id);
+            if (employee != null)
+            {
+                EmployeeDbContext.Employees.Remove(employee);
+                await EmployeeDbContext.SaveChangesAsync();
+
+                return Ok(employee);
+            }
+
+            else
+            {
+                return NotFound("Employee not found");
+            }
+        }
+
+
+
+    }
+}
---
+++ ReportPath/CrudWebAbi/CrudWebAbi/Database/EmployeeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CrudWebAbi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudWebAbi.Database
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
